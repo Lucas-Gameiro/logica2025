fdad164f3ca737c7fcfe0a798a32252a17d2d283
Lucas-Gameiro <lucas.gameiro@unifesp.br>
2025-05-12 13:54:59 -0300

    Add files via upload

---
+++ 1.12.24.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    double n = 1, d = 1, s=0;
+    for(int i=0;i<50;i++){
+        s += n/d;
+        n += 2;
+        d += 1;
+    }
+    printf("%lf",s);
+    return 0;
+}
---
+++ 1153 - Fatorial Simples.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, fat = 1;
+    scanf("%d",&n);
+    for (int i = 0;i<n;i++){
+        fat *= n-i;
+    }
+    printf("%d\n",fat);
+    return 0;
+}
