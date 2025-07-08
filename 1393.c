99c99af941b307f06692a15dd6fa0748e4f54080
Lucas-Gameiro <lucas.gameiro@unifesp.br>
2025-07-08 14:08:50 -0300

    Update 1393.c

--- 1393.c
+++ 1393.c
@@ -8,8 +8,8 @@ int main() {
             continue;
         }
         long long dp[41];
-        dp[1] = 1;  // sorriso -> 1
-        dp[2] = 2;  // sorriso -> 1 -> 2, sorriso -> 2
+        dp[1] = 1;  
+        dp[2] = 2; 
         for (int i = 3; i <= N; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
