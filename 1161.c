8e734875fedc03f59158387aef7a871c10be9ac1
Lucas-Gameiro <lucas.gameiro@unifesp.br>
2025-07-16 11:23:45 -0300

    Update 1161.c

--- 1161.c
+++ 1161.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
- 
-long long fatorial(int n) {
-    if (n == 0) return 1;
-    long long fat = 1;
-    for (int i = 1; i <= n; i++) {
-        fat *= i;
+#include <math.h>
+long long int fatorial(long long int num){
+    if(num==0){
+        return 1;
     }
-    return fat;
+    else{
+        if(num==1){
+            return 1;
+        }
+    }
+    return num*fatorial(num-1);
 }
 
-int main() {
-    int m, n;
-    while (scanf("%d %d", &m, &n) == 2) {
-        long long fatm = fatorial(m);
-        long long fatn = fatorial(n);
-        long long soma = fatm + fatn;
-        printf("%lld\n", soma);
+int main(){
+    long long int a,b;
+    while(scanf("%lld %lld",&a,&b) != EOF){
+        long long int r = fatorial(a) + fatorial(b);
+        printf("%lli\n",r);
     }
     return 0;
 }
